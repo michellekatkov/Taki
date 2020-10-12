01200372bd35fb66c2a08b57d616de88ba1afe65
michellekatkov <61124877+michellekatkov@users.noreply.github.com>
2020-10-12 19:01:48 +0300

    simulator draft coded
    not debugged

--- PlayingAlgorithm/PlayingAlgorithm/Class1.cs
+++ PlayingAlgorithm/PlayingAlgorithm/Class1.cs
@@ -18,6 +18,11 @@ namespace PlayingAlgorithm
         }
         Color myColor;
 
+        public static TakiColor red = new TakiColor(TakiColor.Color.red);
+        public static TakiColor green = new TakiColor(TakiColor.Color.green);
+        public static TakiColor blue = new TakiColor(TakiColor.Color.blue);
+        public static TakiColor yellow = new TakiColor(Color.yellow);
+
         public override bool Equals(object obj)
         {
             if( obj is TakiColor)
@@ -46,11 +51,19 @@ namespace PlayingAlgorithm
         {
             return 542640994 + myColor.GetHashCode();
         }
+        public void CopyColorFrom(TakiColor color)
+        {
+            myColor = color.myColor;
+        }
     }
 
     public class TakiFace
     {
         int num;
+        public TakiFace(int num)
+        {
+            this.num = num;
+        }
         public override string ToString()
         {
             return num.ToString();
@@ -69,7 +82,6 @@ namespace PlayingAlgorithm
             return -992186197 + num.GetHashCode();
         }
     }
-
 
     public class TakiCard
     {
@@ -96,6 +108,10 @@ namespace PlayingAlgorithm
         public override int GetHashCode()
         {
             return 34944597 + type.GetHashCode();
+        }
+        public virtual void CopyCardFrom(TakiCard card)
+        {
+            type = card.type;
         }
     }
 
@@ -140,6 +156,14 @@ namespace PlayingAlgorithm
         {
             this.specialType = specialType;
         }
+
+        public static SpecialTakiCardType stop = new SpecialTakiCardType(SpecialTakiCardType.stop);
+        public static SpecialTakiCardType plus2 = new SpecialTakiCardType(SpecialTakiCardType.plus2);
+        public static SpecialTakiCardType switchDirection = new SpecialTakiCardType(SpecialTakiCardType.switchDirection);
+        public static SpecialTakiCardType switchColor = new SpecialTakiCardType(SpecialTakiCardType.switchColor);
+        public static SpecialTakiCardType taki = new SpecialTakiCardType(SpecialTakiCardType.taki);
+        public static SpecialTakiCardType superTaki = new SpecialTakiCardType(SpecialTakiCardType.superTaki);
+        public static SpecialTakiCardType plus = new SpecialTakiCardType(SpecialTakiCardType.plus);
 
         public class SpecialTakiCardType
         {
@@ -259,6 +283,19 @@ namespace PlayingAlgorithm
             }
             cardsSize = newSize;
         }
+        public void CopyCardsFrom(TakiCardCollection _cards)
+        {
+            if (cardsSize <= _cards.numCards)
+            {
+                cardsSize = (_cards.numCards % maxCardSize == 0) ? _cards.numCards : maxCardSize * (_cards.numCards / maxCardSize + 1);           
+                cards = new TakiCard[cardsSize];
+            }
+            
+            for (int k = 0; k < numCards; k++)
+            {
+                cards[k] = _cards.cards[k];
+            }
+        }
         public void AddCard(TakiCard card)
         {
             _allocateCards(cardsSize + 1);
@@ -275,6 +312,10 @@ namespace PlayingAlgorithm
             {
                 cards[cardIndex] = cards[numCards];
             }
+        }
+        public TakiCard RandomCard()
+        {
+            return cards[MyRandom.randomUniform(numCards)];
         }
         public int FindCard(TakiCard card)
         {
@@ -299,6 +340,15 @@ namespace PlayingAlgorithm
                 cards[ii] = tmp;
             }
         }
+        public TakiCard Pop()
+        {
+            if (numCards == 0)
+            {
+                return null;
+            }
+            numCards--;
+            return cards[numCards];
+        } 
         public bool IsEmpty()
         {
             return numCards == 0;
@@ -316,9 +366,6 @@ namespace PlayingAlgorithm
             this.maxValue = maxValue;
         }
     }
-
-
-
     public class TakiCardRestriction_Color: TakiCardRestriction
     {
         TakiColor color;
@@ -342,17 +389,113 @@ namespace PlayingAlgorithm
 
     public class TakiTable
     {
-        TakiCardCollection discardPile, drawPile;
+        public TakiCardCollection discardPile { get; private set; }
+        public TakiCardCollection drawPile{ get; private set; }
         TakiCard leadingCard;
         TakiColor actionColor;
         bool takiAction, specialAction;
         bool direction = true; // forward
         int plus2amount=0; // should be in construction and in the beggining of game
-        Player[] players;
+        public Player[] players;
+        int numPlayers, addedPlayers;
+
+        public TakiTable(int numPlayers)
+        {
+            this.numPlayers = numPlayers;
+            addedPlayers = 0;
+            players = new Player[numPlayers];
+        }
 
         /** 
          * play one turn of the game return if game is finished
          */
+        public static TakiCardCollection takiDeck = GenerateTakiDeck();
+        public static TakiCardCollection GenerateTakiDeck()
+        {
+            //change to real num
+            int numRepetitions = 2;
+            int specialRepetitions = 4;
+
+            TakiCardCollection takiDeck = new TakiCardCollection();
+            // generate numericCards and coloredSpecialCards
+            NumericTakiCard numCard;
+            SpecialTakiCard specialCard;
+            for (int i = 0; i < numRepetitions; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    TakiFace face = new TakiFace(j);
+                    numCard = new NumericTakiCard(face, TakiColor.blue);
+                    takiDeck.AddCard(numCard);
+                    numCard = new NumericTakiCard(face, TakiColor.red);
+                    takiDeck.AddCard(numCard);
+                    numCard = new NumericTakiCard(face, TakiColor.green);
+                    takiDeck.AddCard(numCard);
+                    numCard = new NumericTakiCard(face, TakiColor.yellow);
+                    takiDeck.AddCard(numCard);
+                }
+                AddColorsFor(SpecialTakiCard.stop, takiDeck);
+                AddColorsFor(SpecialTakiCard.taki, takiDeck);
+                AddColorsFor(SpecialTakiCard.plus, takiDeck);
+                AddColorsFor(SpecialTakiCard.plus2, takiDeck);
+                AddColorsFor(SpecialTakiCard.switchDirection, takiDeck);
+                specialCard = new SpecialTakiCard(SpecialTakiCard.superTaki);
+                takiDeck.AddCard(specialCard);
+            }
+            // generate 4x switch color
+            for (int k = 0; k < specialRepetitions; k++)
+            {
+                specialCard = new SpecialTakiCard(SpecialTakiCard.switchColor);
+                takiDeck.AddCard(specialCard);
+            }
+            return takiDeck;
+        }
+        private static void AddColorsFor(SpecialTakiCard.SpecialTakiCardType type, TakiCardCollection takiDeck)
+        {
+            ColoredSpecialTakiCard coloredSpecialCard;
+            coloredSpecialCard = new ColoredSpecialTakiCard(type, TakiColor.blue);
+            takiDeck.AddCard(coloredSpecialCard);
+            coloredSpecialCard = new ColoredSpecialTakiCard(type, TakiColor.red);
+            takiDeck.AddCard(coloredSpecialCard);
+            coloredSpecialCard = new ColoredSpecialTakiCard(type, TakiColor.green);
+            takiDeck.AddCard(coloredSpecialCard);
+            coloredSpecialCard = new ColoredSpecialTakiCard(type, TakiColor.yellow);
+            takiDeck.AddCard(coloredSpecialCard);
+        }
+        public TakiCard DrawOneCard()
+        {
+            TakiCard card = drawPile.Pop();
+            if (drawPile.IsEmpty())
+            {
+                TakiCardCollection tmp = discardPile;
+                discardPile = drawPile;
+                drawPile = discardPile;
+                drawPile.Shuffle();
+            }
+            return card;
+        }
+        public void CopyTableFrom(TakiTable table)
+        {
+            discardPile.CopyCardsFrom(table.discardPile);
+            drawPile.CopyCardsFrom(table.drawPile);
+            leadingCard.CopyCardFrom(table.leadingCard);
+            actionColor.CopyColorFrom(table.actionColor);
+            takiAction = table.takiAction;
+            specialAction = table.specialAction;
+            direction = table.direction;
+            plus2amount = table.plus2amount;
+            numPlayers = table.numPlayers;
+            for (int i = 0; i < numPlayers; i++)
+            {
+                players[i].CopyPlayerFrom(table.players[i]);
+            }
+        }
+
+        public void AddPlayer(Player player)
+        {
+            players[addedPlayers] = player;
+            addedPlayers++;
+        }
         public bool PlayTurn()
         {
             TakiCard move;
@@ -379,25 +522,10 @@ namespace PlayingAlgorithm
                         while (plus2amount > 0)
                         {
                             plus2amount--;
-                            players[i].DrawOneCard(drawPile.cards[0]);
-                            drawPile.RemoveCard(0);
-                            if (drawPile.IsEmpty())
-                            {
-                                TakiCardCollection tmp = discardPile;
-                                discardPile = drawPile;
-                                drawPile = discardPile;
-                                drawPile.Shuffle();
-                            }
+                           
+                            players[i].DrawOneCard(DrawOneCard());                           
                         }
-                        players[i].DrawOneCard(drawPile.cards[0]);
-                        drawPile.RemoveCard(0);
-                        if (drawPile.IsEmpty())
-                        {
-                            TakiCardCollection tmp = discardPile;
-                            discardPile = drawPile;
-                            drawPile = discardPile;
-                            drawPile.Shuffle();
-                        }
+                        players[i].DrawOneCard(DrawOneCard());
                     }
                     else
                     {
@@ -504,16 +632,28 @@ namespace PlayingAlgorithm
 
     public class Player
     {
-        TakiCardCollection hand;
+        TakiCardCollection canPlay;
+        public enum PlayerState
+        {
+            specificMove,
+            randomMove
+        }
+        public PlayerState currentPlayerState;
+        public TakiCard specificCardToPlay;
+
+        public TakiCardCollection hand;
         public Player()
         {
             hand = new TakiCardCollection();
+            currentPlayerState = PlayerState.randomMove;
+            canPlay = new TakiCardCollection();
         }
         public void DrawOneCard(TakiCard card)
         {
             hand.AddCard(card);
         }
         public TakiColor SwitchColorTo()
+
         {
             // TODO: implement
             return null;
@@ -525,21 +665,143 @@ namespace PlayingAlgorithm
              */
         public TakiCard PlayCard(TakiTable table)            
         {
+            if( currentPlayerState == PlayerState.specificMove)
+            {
+                return specificCardToPlay;
+            }
+            
             for (int cardIndex=0; cardIndex < hand.cards.Length; cardIndex++)
             {
-                if 
-                    (table.CanPlay(hand.cards[cardIndex]))
+                if (table.CanPlay(hand.cards[cardIndex]))
                 {
-                    return hand.cards[cardIndex];
+                    canPlay.AddCard(hand.cards[cardIndex]);
                 }
             }
-            return null;
+            TakiCard randomCard = canPlay.RandomCard();
+            return randomCard;
+        }
+        public void CopyPlayerFrom(Player player)
+        {
+            hand.CopyCardsFrom(player.hand);
         }
         public bool ShouldFinishTakiAction()
         {
             //TODO: implement
             return false;
         }
+    }
+    
+    public class TakiSimulation
+    {
+
+        TakiTable game, // current table on server 
+            moveTable; // table for checking a move
+        Player me; // the player that wants to win
+
+        public TakiSimulation(int numPlayers)
+        {
+            // generate full collection of cards
+            game.drawPile.CopyCardsFrom(TakiTable.takiDeck);
+            game.drawPile.Shuffle();
+            // draw cards to my player
+            Player me = new Player();
+            game.AddPlayer(me);
+            this.me = me;
+            moveTable = new TakiTable(numPlayers-1);
+            for (int i = 0; i < numPlayers-1; i++)
+            {
+                game.AddPlayer(new Player());
+            }
+        }
+        public static TakiSimulation LoadGame()
+        {
+            return new TakiSimulation(1);
+        }
+
+        /**
+         * in the game we only know our hand, discard pile and leading card.
+         * we do not know other hands
+         * to simulate other hands we keep all cards that are not discarded and not in our hand in a drawPile
+         * and then each turn draw cards to other hands according to constraints.
+        */
+
+        public static TakiSimulation GenerateRandomGame()
+        {
+            TakiSimulation sim = new TakiSimulation(4);
+            // draw cards for my player
+            for (int cardIndex = 0; cardIndex < 8; cardIndex++)
+            {
+                sim.me.DrawOneCard(sim.game.DrawOneCard());
+            }            
+            // draw cards to pile to simulate first round before us.
+            // TODO:check first special leading card
+            TakiCard leadingCard = sim.game.drawPile.Pop();
+            sim.game.discardPile.AddCard(leadingCard);
+            return sim;
+        }
+        public void SimulateNextMove()
+        {
+            int numCardsIcanPlay = 0;
+            TakiCardCollection canPlay = new TakiCardCollection();
+            for (int i = 0; i < me.hand.numCards; i++)
+            {
+                if (game.CanPlay(me.hand.cards[i]))
+                {
+                    numCardsIcanPlay++;
+                    canPlay.AddCard(me.hand.cards[i]);
+                }
+            }
+
+            // we do it here once, but should do it several times (10000) each time shuffling drawPile and 
+            // drawing cards to other palyers
+            float[] moveCost = new float[numCardsIcanPlay];
+            for (int mvInd = 0; mvInd < numCardsIcanPlay; mvInd++)
+            {
+                moveCost[mvInd] = SimulateMove(canPlay.cards[mvInd]);
+                // do not forget to update moveTable after each turn
+                // or copy it from game when rolling back
+            }
+        }
+
+        public float SimulateMove(TakiCard move)
+        {
+            moveTable.CopyTableFrom(game);
+            me.currentPlayerState = Player.PlayerState.specificMove;
+            me.specificCardToPlay = move;
+            moveTable.PlayTurn();
+
+            me.currentPlayerState = Player.PlayerState.randomMove;
+
+            int depth = 5; // should do stratified depth depending on move cost
+            // i.e. look deeper for moves that are good and shallow for not so good moves
+            for( int turn=0; turn< depth; turn++)
+            {
+                moveTable.PlayTurn();
+            }
+            // computing cost here
+            /**
+             * should have 1 when winning
+             * should have 0 if someone won (should play turn returns whether game ended?)
+             * So ???
+             * ?? ratio of my cards vs best player?
+             * nCards(bestPlayer)/(nCards(me)+nCards(bestPlayer)); nm= nCards(me), nb= nCards(bestPlayer)
+             * if nb=0 => 0/(0+??)= 0 we lost
+             * if nm=0 => ?/(?+0)= 1 we wAn
+             * if nm= nb => n/(n+n)= 1/2
+             */
+            int nm = game.players[0].hand.numCards;
+            int nb = game.players[1].hand.numCards;
+            for (int player = 2; player < game.players.Length; player++)
+            {
+                int tmp = game.players[player].hand.numCards; 
+                if ( tmp< nb)
+                {
+                    nb = tmp;
+                }
+            }
+            return ((float)nb) / ((float)(nm + nb));
+        }
+
     }
 }
 
